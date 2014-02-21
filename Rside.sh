7be710a60213fc3d8bc90a73ff18a6123fa73879
Thomas-Scott <thomas@entagen.com>
2014-02-20 20:23:51 -0600

    Delete Rside.sh

--- Rside.sh
+++
@@ -1,6 +0,0 @@
-R CMD INSTALL StochLib
-##R -f "StochLib Test.R"
-R -f "parallelTest.R"
-echo "NOTES:"
-echo "~/ contains output folder. Will update later."
-echo "If build failed try changing ~/.R/Makevars to include the following: CXX = g++-4.2"
